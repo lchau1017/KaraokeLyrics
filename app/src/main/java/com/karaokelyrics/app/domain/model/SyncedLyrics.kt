4e92a0b87af568304a9281178d13069e2d4b8519
lung <lchau1017@gmail.com>
2026-02-25 13:31:40 +0000

    Clean

--- app/src/main/java/com/karaokelyrics/app/domain/model/SyncedLyrics.kt
+++ app/src/main/java/com/karaokelyrics/app/domain/model/SyncedLyrics.kt
@@ -1,3 +1,3 @@
 package com.karaokelyrics.app.domain.model
 
-data class SyncedLyrics(val lines: List<LyricsLine>, val metadata: Map<String, String> = emptyMap())
+data class SyncedLyrics(val lines: List<LyricsLine>)
